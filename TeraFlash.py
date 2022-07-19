cccf4bf1c70e2fcb99698299a5b69428903ff74c
Zhenisbek Tagay <40364720+tagay@users.noreply.github.com>
2022-07-19 16:31:29 -0400

    Update TeraFlash.py

--- TeraFlash.py
+++ TeraFlash.py
@@ -134,6 +134,11 @@ def get_signal_and_fft(filename, peak_pos, width, pad_size, N_FFT):
 
 
 def get_signal_and_fft_wide(filename, peak_pos, width, pad_size, offset, N_FFT):
+    print("Peak pos is: "+str(peak_pos))
+    print("Width is: "+str(width))
+    print("Pad size is: "+str(pad_size))
+    print("offset is: "+str(offset))
+    
     sig_x=read_data(filename,1)
     sig_y=-read_data(filename,3)
 
